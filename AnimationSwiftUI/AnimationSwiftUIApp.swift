847e4116f0ed00ba13c0e969bbaa159f44cd3ba6
BorisSonin <bsonin85@gmail.com>
2023-01-28 13:08:33 +0300

    Initial Commit

---
+++ AnimationSwiftUI/AnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftUIApp.swift
+//  AnimationSwiftUI
+//
+//  Created by Борис Сонин on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
