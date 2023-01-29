019e59d6b465c4d5b491b6466b852c2af2899b2c
BorisSonin <bsonin85@gmail.com>
2023-01-29 19:59:03 +0300

    Added a background and an object with rotation animation

--- AnimationSwiftUI/ContentView.swift
+++ AnimationSwiftUI/ContentView.swift
@@ -8,19 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isRotated = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.blue, .green], startPoint: isRotated ? .topLeading : .bottomLeading, endPoint: isRotated ? .bottomTrailing : .topTrailing)
+                .ignoresSafeArea()
+                .onAppear { withAnimation((.linear(duration: 2.0).repeatForever(autoreverses: true)))
+                    { isRotated.toggle()
+                    }
+                }
+            
+            VStack {
+                Text("Tap the image")
+                    .font(.title)
+                    .bold()
+                Spacer()
+                
+                Image(systemName: "sun.max.fill")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .foregroundColor(.yellow)
+                    .rotationEffect(Angle.degrees(isRotated ? 360 : 0))
+                    .rotation3DEffect(Angle.degrees(isRotated ? 180 : 0), axis: (x: 1, y: 0, z: 0))
+                    .animation(.easeInOut(duration: 3), value: isRotated)
+                Spacer()
+                
+            }
+            .padding()
+            .onTapGesture {
+                isRotated.toggle()
+            }
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
